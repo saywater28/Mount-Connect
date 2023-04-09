79e010c9732e4f24ab01488de5d69ad61e2e0059
Sejal <21bec046@nith.ac.in>
2023-04-09 15:45:48 +0530

    messaging functionality added

--- app/src/main/java/com/example/mountconnect/chatMessages.kt
+++ app/src/main/java/com/example/mountconnect/chatMessages.kt
@@ -2,10 +2,26 @@ package com.example.mountconnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 
 class chatMessages : AppCompatActivity() {
+
+    private lateinit var chatRecyclerView: RecyclerView
+    private lateinit var messageBox: EditText
+    private lateinit var sendButton: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_messages)
+
+
+        chatRecyclerView = findViewById(R.id.chatRecyclerView)
+        messageBox = findViewById(R.id.messageBox)
+        sendButton = findViewById(R.id.sentButton)
     }
 }
