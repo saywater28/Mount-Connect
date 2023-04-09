146ebd90cf21db1f54f30f0549014506e7af5e47
Sejal <21bec046@nith.ac.in>
2023-04-09 14:44:09 +0530

    messaging functionality added

---
+++ app/src/main/java/com/example/mountconnect/chatMessages.kt
@@ -0,0 +1,11 @@
+package com.example.mountconnect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class chatMessages : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_messages)
+    }
+}
