48b0356736b099794117e736a69e72f4a3b5faef
Sejal <21bec046@nith.ac.in>
2023-04-09 15:08:50 +0530

    messaging functionality added

--- app/src/main/java/com/example/mountconnect/Message.kt
+++ app/src/main/java/com/example/mountconnect/Message.kt
@@ -1,4 +1,14 @@
 package com.example.mountconnect
 
 class Message {
+    var message: String? = null
+    var senderId : String? = null
+
+    constructor(){}
+
+    constructor(message: String?, senderId: String?){
+        this.message = message
+        this.senderId = senderId
+    }
+
 }
